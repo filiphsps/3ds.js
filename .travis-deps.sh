1132e562f694c162b3cbcb6e1424781d0824983d
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-10-21 01:34:16 +0200

    Test without sudo

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -5,11 +5,11 @@ set -x
 # Build and install devkitARM + ctrulib
 wget http://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl
 git clone https://github.com/smealum/ctrulib.git
-sudo perl devkitARMupdate.pl
+perl devkitARMupdate.pl
 
 # Get latest ctrulib and overwrite bundled one
 cd ctrulib/libctru && make ; cd -
-sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
+cp -rf ctrulib/libctru/ ${DEVKITPRO}
 
 # Get, Build & Install dependencies
 
